7906066b3bbf63ae9fb57354488099513e277b1b
Rafael Carmona <rafaelc.carmona@gmail.com>
2021-06-04 21:50:02 -0500

    archivo base de datos y archivo

---
+++ database.py
@@ -0,0 +1,53 @@
+import hashlib
+from peewee  import *
+from datetime import datetime
+
+database = MySQLDatabase('fastapi_project', 
+                          user='root', 
+                          password='', 
+                          host='127.0.0.1',
+                          port=3306)
+
+class User(Model):
+    username = CharField(max_length=50, unique=True)
+    password = CharField(max_length=50)
+    created_at = DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        database= database
+        table_name = 'users'
+
+    @classmethod
+    def create_password(cls, password):
+
+        h= hashlib.md5()
+        h.update(password.encode('utf-8'))
+        return h.hexdigest()
+
+
+class Movie(Model):
+    title = CharField(max_length=50)
+    created_at=DateTimeField(default=datetime.now)
+    def _str_(self):
+        return self.username
+
+    class Meta:
+        database=database
+        table_name = 'movies'
+
+class UserReview(Model):
+    user=ForeignKeyField(User, backref='reviews')
+    movie=ForeignKeyField(Movie, backref='reviews')
+    review= TextField()
+    score=IntegerField()
+    created_at= DateTimeField(default=datetime.now)
+
+    def __str__(sefl):
+        return f'{self.username}-{self.movie.title}'
+    class Meta:
+        database= database
+        table_name= 'reviews'
+
